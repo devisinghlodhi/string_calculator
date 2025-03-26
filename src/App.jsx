faf36fd1a445a9844b0990dbf191b870e13930fe
devisingh.lodhi <devisinghlodhi1999@gmail.com>
2025-03-26 23:25:27 +0530

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";  // ✅ Import Routes and Route
+import Home from './pages/home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline">
+                String Calculator
+            </h1>
+        </>
+    )
+}
